31e31f1edacfe4bf0b7e5b7863b28d8180f4defd
mkreddy477 <mkreddy.477@gmail.com>
2025-12-14 17:47:06 +0530

    security file changes done.

--- src/main/java/com/example/travelhub/config/SecurityConfig.java
+++ src/main/java/com/example/travelhub/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.travelhub.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.config.Customizer;
 
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 	
 	  @Bean
